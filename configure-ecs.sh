54b0db8c0f64696dd39ee731e966b163a5915fb3
LJay79 <github@jens79.de>
2018-07-10 11:28:04 +0200

    allow changing of AWS Region, allow passing in ECS template URL

--- configure-ecs.sh
+++ configure-ecs.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-set -e
+set -ea
+source .env
 
 echo "In configure-ecs.sh"
 script_dir="$(dirname "$0")"
 bin_dir="$(dirname $0)/../bin"
+
+echo AWS Region = ${AWS_REGION}
 
 echo The value of arg 0 = $0
 echo The value of arg 1 = $1
@@ -11,7 +14,7 @@ echo The value of arg 2 = $2
 echo The value of arg 3 = $3 
 echo The value of arg 4 = $4  
 echo The value of arg script_dir = $script_dir
-echo UPDATED 201606072316
+echo UPDATED 201807091552
 
 MY_STACK=$1
 MY_ACCTID=$2
@@ -27,18 +30,18 @@ uuid=$(date +%s)
 awsacctid="$MY_ACCTID"
 ecr_repo="$MY_ECR"
 ecs_stack_name="$MY_STACK"
-
-ecs_template_url="https://s3.amazonaws.com/stelligent-training-public/public/codepipeline/ecs-pipeline.json"
-#ecs_template_url="$MY_URL"
+ecs_template_url="$MY_URL"
 
 echo The value of arg uuid = $uuid
-eval $(aws --region us-east-1 ecr get-login)
+# remove "-e none", causes error "unknown option -e"
+# use hostname only, remote optional https
+eval $(aws ecr get-login --region ${AWS_REGION} | sed 's|https://||' | sed -e 's/-e none//g')
 
 # Build, Tag and Deploy Docker
 docker build -t $ecr_repo:$uuid .
-docker tag $ecr_repo:$uuid $awsacctid.dkr.ecr.us-east-1.amazonaws.com/$ecr_repo:$uuid
-docker push $awsacctid.dkr.ecr.us-east-1.amazonaws.com/$ecr_repo:$uuid
+docker tag $ecr_repo:$uuid $awsacctid.dkr.ecr.${AWS_REGION}.amazonaws.com/$ecr_repo:$uuid
+docker push $awsacctid.dkr.ecr.${AWS_REGION}.amazonaws.com/$ecr_repo:$uuid
 
-aws cloudformation update-stack --stack-name $ecs_stack_name --template-url $ecs_template_url --region us-east-1 --capabilities="CAPABILITY_IAM" --parameters ParameterKey=AppName,UsePreviousValue=true ParameterKey=ECSRepoName,UsePreviousValue=true ParameterKey=DesiredCapacity,UsePreviousValue=true ParameterKey=KeyName,UsePreviousValue=true ParameterKey=RepositoryBranch,UsePreviousValue=true ParameterKey=RepositoryName,UsePreviousValue=true ParameterKey=InstanceType,UsePreviousValue=true ParameterKey=MaxSize,UsePreviousValue=true ParameterKey=S3ArtifactBucket,UsePreviousValue=true ParameterKey=S3ArtifactObject,UsePreviousValue=true ParameterKey=SSHLocation,UsePreviousValue=true ParameterKey=YourIP,UsePreviousValue=true ParameterKey=ImageTag,ParameterValue=$uuid ParameterKey=ECSCFNURL,ParameterValue=$ecs_template_url
+aws cloudformation update-stack --stack-name $ecs_stack_name --template-url $ecs_template_url --region ${AWS_REGION} --capabilities="CAPABILITY_IAM" --parameters ParameterKey=AppName,UsePreviousValue=true ParameterKey=ECSRepoName,UsePreviousValue=true ParameterKey=DesiredCapacity,UsePreviousValue=true ParameterKey=KeyName,UsePreviousValue=true ParameterKey=RepositoryBranch,UsePreviousValue=true ParameterKey=RepositoryName,UsePreviousValue=true ParameterKey=InstanceType,UsePreviousValue=true ParameterKey=MaxSize,UsePreviousValue=true ParameterKey=S3ArtifactBucket,UsePreviousValue=true ParameterKey=S3ArtifactObject,UsePreviousValue=true ParameterKey=SSHLocation,UsePreviousValue=true ParameterKey=YourIP,UsePreviousValue=true ParameterKey=ImageTag,ParameterValue=$uuid ParameterKey=ECSCFNURL,ParameterValue=$ecs_template_url
 
 sleep 10
